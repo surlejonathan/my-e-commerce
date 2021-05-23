dea8c5b2e2a3d74500636b8c8295cc792a10bffa
surlejonathan <54603496+surlejonathan@users.noreply.github.com>
2021-05-24 01:44:11 +0200

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,8 +1,1 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDPLlWvyPgfjIIR7Bw30-N_kyrFli93ycM",
-  authDomain: "my-e-commerce-10fe8.firebaseapp.com",
-  projectId: "my-e-commerce-10fe8",
-  storageBucket: "my-e-commerce-10fe8.appspot.com",
-  messagingSenderId: "168996772367",
-  appId: "1:168996772367:web:370933b3edd0421c53e17a",
-};
+
